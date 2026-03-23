2aed4ccb315c53c13fe9f02f870fc54e60e6f1ae
yomso-imnida <dashkan2003@gmail.com>
2026-03-23 18:33:36 +0300

    Classwork 23.03

---
+++ 20260323/0/httpsrv.py
@@ -0,0 +1,9 @@
+import http.server
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+print(s.getsockname()[0])
+s.close()
+
+http.server.test(http.server.SimpleHTTPRequestHandler, port=8000)
